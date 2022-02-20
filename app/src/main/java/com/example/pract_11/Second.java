7d62e2e08959624afdbf74298a1bb7d97f09fc73
kolodeznikov <dkmori@mail.ru>
2022-02-20 20:36:40 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pract_11/Second.java
@@ -0,0 +1,13 @@
+package com.example.pract_11;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+    }
+}
